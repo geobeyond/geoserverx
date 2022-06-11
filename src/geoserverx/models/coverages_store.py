a40fc7b1ca1369f6796566f2e77b61ddec2801d1
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2022-06-11 16:41:22 +0200

    Remove connection parameters

--- src/geoserverx/models/coverages_store.py
+++ src/geoserverx/models/coverages_store.py
@@ -1,29 +1,30 @@
 from typing import Dict, List
 from pydantic import BaseModel
-from pyparsing import Optional
+from typing import Optional
 
-from .workspace import WorkspaceInBulk
-# from . import WorkspaceInBulk
+from geoserverx.models.workspace import WorkspaceInBulk
 
 
 class CoveragesStoreInBulk(BaseModel):
 	name: str
 	href: str
 
+
 class CoveragesStoresDict(BaseModel):
 	coverageStore: List[CoveragesStoreInBulk]
 
+
 class CoveragesStoresModel(BaseModel):
 	coverageStores = CoveragesStoresDict
+
 
 class CoveragesStoreModel(BaseModel):
 	name: str
 	description: str
 	enabled: bool
 	workspace: WorkspaceInBulk
-	connectionParameters: Optional[Dict] 
 	_default: bool
 	url: str
 	coverages: str
 	dateCreated: str
-	metadata:Optional[Dict] 
+	metadata: Optional[Dict] 
