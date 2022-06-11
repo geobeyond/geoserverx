a1457b275c1b4f19509bbcdebab44ebf35349197
Francesco Bartoli <francesco.bartoli@geobeyond.it>
2022-06-11 18:47:03 +0200

    Hotfix/handle conflicts add protocol (#11)
    * rebased
    
    * removed unnecessary functions
    
    * Remove connection parameters
    
    * Add a logger as a dependency
    
    
    Add a logger as a dependency
    
    
    Add a logger as a dependency
    
    Co-authored-by: krishnaglodha <krishnaglodha@gmail.com>

--- src/geoserverx/Utils/Services/datastore.py
+++ src/geoserverx/Utils/Services/datastore.py
@@ -1,21 +1,27 @@
-
-   
 import json
-import time
 from logging import Logger
 from typing import Protocol
 
 
 class AddDataStoreProtocol(Protocol):
     """
-    Represents functionality of adding two numbers.
+    Represents functionality of sending a file to the server.
     """
-    def addFile(self, client,  workspace, store, method, store_payload,layer_payload, store_header,layer_header):
+    def addFile(
+        self, client, workspace, store, method,
+        store_payload, layer_payload, store_header,
+        layer_header
+    ):
         ...
 
+
 class CreateFileStore:
-    def addFile(self, client,  workspace, store, method, store_payload,layer_payload, store_header,layer_header):
-         with client as Client:
+    def addFile(
+        self, client, workspace, store, method,
+        store_payload, layer_payload, store_header,
+        layer_header
+    ):
+        with client as Client:
             store_responses = Client.post(
                 f"workspaces/{workspace}/datastores/", data=store_payload, headers=store_header
             )
@@ -26,16 +32,20 @@ class CreateFileStore:
             )
             results = layer_responses.status_code
             return results
-        
-
 
 
 class ShapefileStore:
-    def __init__(self, service:AddDataStoreProtocol, file) -> None:
+    def __init__(
+        self,
+        service: AddDataStoreProtocol,
+        logger: Logger,
+        file
+    ) -> None:
         self.inner = service
+        self.logger = logger
         self.file = file
 
-    def addFile(self,client,workspace,store):
+    def addFile(self, client, workspace, store):
         store_payload: str = json.dumps(
             {
                 "dataStore": {
@@ -46,20 +56,29 @@ class ShapefileStore:
                 }
             }
         )
-        print(store_payload)
+        self.logger.debug(f"Shapefile store payload: {store_payload}")
         layer_payload = self.file
         result = self.inner.addFile(
-            client,  workspace, store, "shp", store_payload,layer_payload, {"Content-Type": "application/json"}, {"Content-Type": "application/zip"}
+            client, workspace, store, "shp",
+            store_payload, layer_payload,
+            {"Content-Type": "application/json"},
+            {"Content-Type": "application/zip"}
         )
         return result
 
 
-
 class GPKGfileStore:
-    def __init__(self, service:AddDataStoreProtocol,file) -> None:
+    def __init__(
+        self,
+        service: AddDataStoreProtocol,
+        logger: Logger,
+        file
+    ) -> None:
         self.inner = service
+        self.logger = logger
         self.file = file
-    def addFile(self,client,workspace,store):
+
+    def addFile(self, client, workspace, store):
         store_payload: str = json.dumps(
             {
                 "dataStore": {
@@ -73,9 +92,12 @@ class GPKGfileStore:
                 }
             }
         )
-        print(store_payload)
+        self.logger.debug(f"GeoPackage store payload: {store_payload}")
         layer_payload = self.file
         result = self.inner.addFile(
-            client,  workspace, store, "gpkg", store_payload,layer_payload, {"Content-Type": "application/json"}, {"Content-Type": "application/json"}
+            client, workspace, store, "gpkg",
+            store_payload, layer_payload,
+            {"Content-Type": "application/json"},
+            {"Content-Type": "application/json"}
         )
         return result
