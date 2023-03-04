587b08ed4894a6267f3b55ed56211a50b0f7159b
krishnaglodha <krishnaglodha@gmail.com>
2023-03-04 22:05:42 +0530

    fixed json response for create workspace

--- src/geoserverx/cli/cli.py
+++ src/geoserverx/cli/cli.py
@@ -99,8 +99,7 @@ def create_workspace(
     if request.value == "sync":
         client = SyncGeoServerX(username, password, url)
         result = client.create_workspace(workspace, default, isolated).json()
-        print(type(result))
-        if result.code == 201:
+        if json.loads(result)['code'] == 201:
             typer.secho(result, fg=typer.colors.GREEN)
         else:
             typer.secho(result, fg=typer.colors.RED)
