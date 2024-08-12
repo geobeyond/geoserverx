69b26c1db66df17542217c38bc159aff2cbbfe00
Krishna Lodha <47075664+krishnaglodha@users.noreply.github.com>
2024-08-13 03:08:08 +0530

    fixed all pydantic calls (#51)
    * fixed all pydantic calls
    
    * upgraded constraints file
    
    * updated version of checkout and python in test.yml
    
    * updated poetry version in contraints
    
    * trying poetry installer
    
    * updated poetry installation method in release.yml

--- src/geoserverx/models/data_store.py
+++ src/geoserverx/models/data_store.py
@@ -22,7 +22,7 @@ class DatastoreConnection(BaseModel):
     path: str = Field(..., alias="$")
 
     class Config:
-        allow_population_by_field_name = True
+        populate_by_name = True
 
 
 class EntryItem(BaseModel):
