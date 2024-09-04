a380a2d565ce315419cd5057c5541040abe3a792
Krishna Lodha <47075664+krishnaglodha@users.noreply.github.com>
2024-09-04 16:20:01 +0530

    using black isort and ruff in the test CI pipeline (#63)
    * using black isort and ruff in the test CI pipeline
    
    * removed duplicate installation
    
    * testing with black and isort marketplace options
    
    * testing black action
    
    * testing with ruff
    
    * fixed black .
    
    * updated black and ruff version and run black again
    
    * fixed as per isort
    
    * added profile black to isort in CI
    
    * testing if ruff gives error in CI
    
    * fixed black but keeping error as per ruff
    
    * fixed as per ruff

--- src/geoserverx/models/layer_group.py
+++ src/geoserverx/models/layer_group.py
@@ -1,5 +1,6 @@
 from enum import Enum
-from typing import List, Literal, Union, Optional
+from typing import List, Literal, Optional, Union
+
 from pydantic import BaseModel, Field
 
 
@@ -7,26 +8,33 @@ class LayerGroupElement(BaseModel):
     name: str = ...
     href: str = ...
 
+
 class LayerGroupList(BaseModel):
     layerGroup: List[LayerGroupElement] = ...
 
+
 class LayerGroupsModel(BaseModel):
     layerGroups: Union[LayerGroupList, Literal[""]]
+
 
 class Published(BaseModel):
     type: str = Field(..., alias="@type")
     name: str = ...
     href: str = ...
 
+
 class Publishables(BaseModel):
     published: Published = ...
+
 
 class Style(BaseModel):
     name: str = ...
     href: str = ...
 
+
 class Styles(BaseModel):
     style: Style = ...
+
 
 class Bounds(BaseModel):
     minx: float = ...
@@ -35,6 +43,7 @@ class Bounds(BaseModel):
     maxy: float = ...
     crs: str = ...
 
+
 class ModeEnum(Enum):
     single = "SINGLE"
     opaque_container = "OPAQUE_CONTAINER"
@@ -42,11 +51,14 @@ class ModeEnum(Enum):
     container = "CONTAINER"
     eo = "EO"
 
+
 class WorkspaceModel(BaseModel):
     name: str = None
 
+
 class BaseLayerGroup(BaseModel):
     name: str = ...
+
 
 class SingleLayerGroup(BaseLayerGroup):
     mode: ModeEnum
@@ -57,11 +69,14 @@ class SingleLayerGroup(BaseLayerGroup):
     bounds: Bounds
     dateCreated: str = ...
 
+
 class SingleLayerGroupModel(BaseModel):
     layerGroup: SingleLayerGroup
 
+
 class LayerListModel(BaseModel):
     layer: List[str] = []
+
 
 class LayerGroupModel(BaseModel):
     name: str
@@ -71,11 +86,14 @@ class LayerGroupModel(BaseModel):
     abstractTxt: Optional[str] = None
     workspace: Optional[WorkspaceModel] = None
 
+
 class LayerGroupPayload(BaseModel):
     layerGroup: LayerGroupModel
 
+
 class LayerGroupStylesModel(BaseModel):
     style: List[str] = []
+
 
 class LayerGroupKeywordsModel(BaseModel):
     keyword: List[str] = []
--- src/geoserverx/utils/enums.py
+++ src/geoserverx/utils/enums.py
@@ -1,4 +1,5 @@
 from enum import Enum
+
 from geoserverx.models.gs_response import GSResponse
 
 
