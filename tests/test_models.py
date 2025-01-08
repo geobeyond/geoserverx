39af440d9b9e17828b961281817df0c8dfbe5045
Ricardo Garcia Silva <ricardo.garcia.silva@gmail.com>
2025-01-08 10:11:25 +0000

    fixed failing test collection

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,13 +1,13 @@
 import pytest
 from pydantic import ValidationError
 
-from ..src.geoserverx.models.coverages_store import (
+from geoserverx.models.coverages_store import (
     CoveragesStoreInBulk,
     CoveragesStoreModel,
     CoveragesStoresDict,
     CoveragesStoresModel,
 )
-from ..src.geoserverx.models.data_store import (
+from geoserverx.models.data_store import (
     DatastoreConnection,
     DataStoreDict,
     DataStoreInBulk,
@@ -16,16 +16,16 @@ from ..src.geoserverx.models.data_store import (
     DataStoresModel,
     EntryItem,
 )
-from ..src.geoserverx.models.geofence import RulesResponse
-from ..src.geoserverx.models.layer_group import LayerGroupsModel
-from ..src.geoserverx.models.style import (
+from geoserverx.models.geofence import RulesResponse
+from geoserverx.models.layer_group import LayerGroupsModel
+from geoserverx.models.style import (
     AllStylesModel,
     SingleStyle,
     StyleModel,
     allStyle,
     allStyleList,
 )
-from ..src.geoserverx.models.workspace import (
+from geoserverx.models.workspace import (
     NewWorkspace,
     WorkspaceInBulk,
     WorkspaceModel,
